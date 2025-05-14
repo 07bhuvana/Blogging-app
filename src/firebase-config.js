107b058b8edcc48f7681a39d0327256ceba792b1
bhuvaneswari N <bhuvaneswarijo712@gmail.com>
2025-05-14 14:31:18 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"; // Import Firebase Storage
 
 const firebaseConfig = {
-  apiKey: "yourapikey",
-  authDomain: "yourdomainname",
-  projectId: "yourid",
-  storageBucket: "yourprojectname", // ✅ FIXED
-  messagingSenderId: "yourid",
-  appId: "yourid",
-  measurementId: "yourid"
+  apiKey: "AIzaSyC2i49gavioG_h0UZYxvQLcM4aEY0kv2go",
+  authDomain: "blogapp-3fbe5.firebaseapp.com",
+  projectId: "blogapp-3fbe5",
+  storageBucket: "blogapp-3fbe5.appspot.com", // ✅ FIXED
+  messagingSenderId: "425480194955",
+  appId: "1:425480194955:web:eff2b300c6fd4ef035bd8e",
+  measurementId: "G-E7FPSCN63X"
 };
 
 // Initialize Firebase
